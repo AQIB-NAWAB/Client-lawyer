182953d1dfb1530bf5f273269c25a2123b3c133e
Aqib Nawab <aqibnawab1100@gmail.com>
2024-01-06 13:42:25 +0500

    Update jwtToken.js

--- server/utilis/jwtToken.js
+++ server/utilis/jwtToken.js
@@ -1,25 +1,21 @@
-// craeting token and savving cookie
-const sendToken = (user, statusCode, res)=>{
-    const token = user.getJWTToken();
-     
-    // console.log(token);
-    // option for Cookie
-    const options = {
-        httpOnly: true,
-        path: "/api/v1",
-        expires: new Date(
-            Date.now() + 5 * 24 * 60 * 60 * 1000
-        ),
-        secure: true,
-        domain: '.vercel.app'
-    };
-    
- 
-    res.status(statusCode).cookie('token',token,options).json({
-        success:true,
-        user,
-        token
-    });
-}
+const sendToken = (user, statusCode, res) => {
+  const token = user.getJWTToken();
 
-module.exports = sendToken
+// options for cookie setting in vercel 
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure:true,
+    sameSite:"none"
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
+
+module.exports = sendToken;
